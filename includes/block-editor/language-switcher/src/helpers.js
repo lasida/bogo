210a0882e71ad75ada8293ff0d4556b7f3fea87f
lasida <lasidaziz@gmail.com>
2025-06-24 13:41:14 +0000

    Update package dependencies: downgrade @wordpress/scripts to 19.2.4, upgrade @wordpress/icons to 10.9.0, and add flag-icons 7.5.0

--- includes/block-editor/language-switcher/src/helpers.js
+++ includes/block-editor/language-switcher/src/helpers.js
@@ -1,10 +1,16 @@
 export const createShortcode = attributes => {
 	let shortcode = `[bogo]`;
 
-	if ( 'suggestion' === attributes.view ) {
-		shortcode = shortcode.replace( /\]$/,
-			` view="${ attributes.view }"]`
+	if ('suggestion' === attributes.view || 'dropdown' === attributes.view) {
+		shortcode = shortcode.replace(/\]$/,
+			` view="${attributes.view}"]`
 		);
+
+		if ('dropdown' === attributes.view && attributes.displayMode && attributes.displayMode !== 'text-only') {
+			shortcode = shortcode.replace(/\]$/,
+				` display_mode="${attributes.displayMode}"]`
+			);
+		}
 	}
 
 	return shortcode;
